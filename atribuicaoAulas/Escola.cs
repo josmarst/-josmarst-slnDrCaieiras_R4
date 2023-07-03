31595ce73b3483cbed25b6d1be72f1d04e961bfe
Osmar <josmarst@gmsil.com>
2023-07-03 20:52:07 -0300

    Commit Push

--- atribuicaoAulas/Escola.cs
+++ atribuicaoAulas/Escola.cs
@@ -7,7 +7,6 @@ namespace atribuicaoAulas
     [Table("Escola")]
     public partial class Escola
     {
-
         // Teste GIT
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Escola()
