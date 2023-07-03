673243c14a97b0215047786e67bea4431e9a973e
Osmar <josmarst@gmsil.com>
2023-07-03 20:39:09 -0300

    Teste GIT Commit

--- atribuicaoAulas/Escola.cs
+++ atribuicaoAulas/Escola.cs
@@ -7,6 +7,8 @@ namespace atribuicaoAulas
     [Table("Escola")]
     public partial class Escola
     {
+
+        // Teste GIT
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Escola()
         {
